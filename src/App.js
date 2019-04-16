e63f83cf2a6ca097d6337d04d094c65c222990ea
Kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2019-04-16 14:04:07 +0530

    Add a welcome header.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: "" };
+  }
+
+  componentDidMount() {
+    fetch("/greeting")
+      .then(res => res.json())
+      .then(({ message }) => {
+        this.setState({ message });
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App">{this.state.message}</div>;
   }
 }
 
